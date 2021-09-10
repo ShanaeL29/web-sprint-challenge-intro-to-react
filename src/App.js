dbcf914f711c31cbe6339a48706288725871e207
Shanae <sleslie18@gmail.com>
2021-09-10 13:50:09 -0400

    Set up state and data fetch

--- src/App.js
+++ src/App.js
@@ -1,19 +1,34 @@
-import React from 'react';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import axios from "axios";
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characterList, setCharacterList] = useState([]);
+  const [error, setError] = useState("");
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
+  // Fetch characters from the API in an effect hook. Remember, anytime you have a
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios
+      .get(`https://swapi.dev/api/people`)
+      .then((res) => {
+        console.log(res.data);
+        setCharacterList(res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        setError("Uh-Oh! We are working on this issue! Please try again soon!");
+      });
+  }, []);
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">STAR WARS STARS</h1>
     </div>
   );
-}
+};
 
 export default App;
